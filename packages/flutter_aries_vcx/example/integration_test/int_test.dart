06e6465c4e7ff11935d6f7b87a3420f7d520e9b8
Dinesh Srinivasan <dinbtechit@gmail.com>
2023-02-13 14:35:12 -0400

    fix: linux builds - Integration test

---
+++ packages/flutter_aries_vcx/example/integration_test/int_test.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+
+
+
+
+Future<void> pause() => Future.delayed(const Duration(milliseconds: 500));
+
+void main() {
+
+  testWidgets('Searching returns expected titles', (tester) async {
+
+  });
+}

2826eb04eff394196c6703df82f2a75d9c84cbe0
Dinesh Srinivasan <dinbtechit@gmail.com>
2023-02-19 15:12:15 -0400

    change: Commenting out libzmq and libsodium.

--- scripts/build-apple.sh
+++ scripts/build-apple.sh
@@ -96,8 +96,8 @@ if [ "$BUILD_TYPE" == "macos" ]; then
  #build_libsodium
  #build_libzmq
 
- extract_architectures "$(pwd)"/libzmq-ios/dist/ios/lib/libzmq.a libzmq zmq
- extract_architectures "$(pwd)"/libsodium-ios/dist/ios/lib/libsodium.a libsodium sodium
+ #extract_architectures "$(pwd)"/libzmq-ios/dist/ios/lib/libzmq.a libzmq zmq
+ #extract_architectures "$(pwd)"/libsodium-ios/dist/ios/lib/libsodium.a libsodium sodium
  extract_architectures "$(pwd)"/OpenSSL-for-iPhone/lib/libssl.a libssl openssl
  extract_architectures "$(pwd)"/OpenSSL-for-iPhone/lib/libcrypto.a libcrypto openssl
  #extract_architectures "$(pwd)"/OpenSSL-for-iPhone/lib/libssl-iOS-Sim.a libssl openssl-sim

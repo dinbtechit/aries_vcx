928c9add96ab2c4e7ca1cb54f30addd126e45722
Dinesh Srinivasan <dinbtechit@gmail.com>
2023-02-27 18:09:56 -0400

    Feature: exposing all api through ffi

--- packages/flutter_aries_vcx/example/integration_test/int_test.dart
+++ packages/flutter_aries_vcx/example/integration_test/int_test.dart
@@ -6,7 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 Future<void> pause() => Future.delayed(const Duration(milliseconds: 500));
 
 Future<void> main() async {
-  testWidgets('TODO write tests', (tester) async {
+  testWidgets('Create instance', (tester) async {
     var arireVcx = await AriesVcx.defaultInstance;
     arireVcx.agencyClient.updateWebhookUrl(webhookUrl: '');
 
